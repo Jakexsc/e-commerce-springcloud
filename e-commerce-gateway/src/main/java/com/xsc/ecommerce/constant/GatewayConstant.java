ca236ea243858629ea494d43c47ed5e4b9ae51b0
jakesc <649199985@qq.com>
2022-01-03 22:53:02 +0800

    获取Post Body的内容

---
+++ e-commerce-gateway/src/main/java/com/xsc/ecommerce/constant/GatewayConstant.java
@@ -0,0 +1,17 @@
+package com.xsc.ecommerce.constant;
+
+/**
+ * @author Jakexsc
+ * 2022/1/3
+ */
+public class GatewayConstant {
+    public static final String LOGIN_URI = "/e-commerce/login";
+
+    public static final String REGISTER_URI = "/e-commerce/register";
+
+    public static final String AUTHORITY_CENTER_TOKEN_URI =
+            "http://%s:%s/ecommerce-authority-center/authority/token";
+
+    public static final String AUTHORITY_CENTER_REGISTER_URI =
+            "http://%s:%s/ecommerce-authority-center/authority/register";
+}
---
+++ e-commerce-gateway/src/main/java/com/xsc/ecommerce/filter/GlobalCacheRequestBodyFilter.java
@@ -0,0 +1,56 @@
+package com.xsc.ecommerce.filter;
+
+import com.xsc.ecommerce.constant.GatewayConstant;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
+import org.springframework.core.io.buffer.DataBuffer;
+import org.springframework.core.io.buffer.DataBufferUtils;
+import org.springframework.http.server.reactive.ServerHttpRequestDecorator;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+/**
+ * @author Jakexsc
+ * 2022/1/3
+ */
+@Component
+public class GlobalCacheRequestBodyFilter implements GlobalFilter, Ordered {
+    private static final Logger LOG = LoggerFactory.getLogger(GlobalCacheRequestBodyFilter.class);
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        boolean isLoginOrRegister = exchange.getRequest().getURI().getPath().contains(GatewayConstant.LOGIN_URI)
+                || exchange.getRequest().getURI().getPath().contains(GatewayConstant.REGISTER_URI);
+
+        if (exchange.getRequest().getHeaders().getContentType() == null || !isLoginOrRegister) {
+            return chain.filter(exchange);
+        }
+
+        // 拿到Http请求的数据 --> dataBuffer
+        return DataBufferUtils.join(exchange.getRequest().getBody()).flatMap(dataBuffer -> {
+            // 确保缓冲区的数据不被释放
+            DataBufferUtils.retain(dataBuffer);
+            Flux<DataBuffer> cacheFlux = Flux.defer(() ->
+                Flux.just(dataBuffer.slice(0, dataBuffer.readableByteCount())));
+            // 重新包装 serverHttpRequest，重写getBody方法，能够返回数据
+            ServerHttpRequestDecorator serverHttpRequestDecorator = new ServerHttpRequestDecorator(exchange.getRequest()) {
+                @Override
+                public Flux<DataBuffer> getBody() {
+                    return cacheFlux;
+                }
+            };
+            // 将包装之后的serverHttpRequest继续向下传递
+            return chain.filter(exchange.mutate().request(serverHttpRequestDecorator).build());
+        });
+    }
+
+    @Override
+    public int getOrder() {
+        return HIGHEST_PRECEDENCE + 1;
+    }
+}
